fee8dfa151c4e93e7954c04425a3bda791ba3178
arayapudi626 <rayapudianisha@gmail.com>
2025-09-11 03:09:42 -0700

    entering and ouputting array fn complete

---
+++ lab6.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespeace std;
+
+void enterArrayData(double *, const int SIZE);
+void outputArrayData(double * arr, const int SIZE);
+double sumArray(double * arr, const int SIZE);
+
+int main () {}
