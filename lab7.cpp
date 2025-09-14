1496e2ba562b70b7fd07dce0a309c102c3d57db4
arayapudi626 <rayapudianisha@gmail.com>
2025-09-14 09:11:59 -0700

    update

--- lab7.cpp
+++ lab7.cpp
@@ -7,12 +7,22 @@ string*reverseArray(string * arr, const int size);
 void displayArray(string * arr, const int size);
 
 int main (){
-
+    string * names = nullptr;
+    names = new string[SIZE];
+    *(names + 0) = "Janet";
+    *(names + 1) = "Jeffe";
+    *(names + 2) = "Jin";
+    *(names + 3) = "Joe";
+    *(names + 4) = "Junio";
 }
 
 string*reverseArray(string * arr, const int size){
-
-
+    for (int i = 0; i < size; i++){
+        string temp = *(arr + i);
+        *(arr + i) = *(arr+(size - 1 - i));
+        *(arr + (size - 1 - i)) = temp;
+    }
+    return arr;
 }
 
 void displayArray(string * arr, const int size){
