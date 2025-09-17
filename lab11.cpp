f668db4a1b69310721c4d7374c7bf52b5591fe70
arayapudi626 <rayapudianisha@gmail.com>
2025-09-17 06:47:30 -0700

    update

--- lab11.cpp
+++ lab11.cpp
@@ -2,11 +2,14 @@
 #include <string>
 using namespace std;
 
+const int COUNTRIES = 3;
+
 struct Country {
     string name;
     double flightPrice;
     int duration;
     string*favSights;
+    int numSights;
 
     ~Country(){
         if (favSights)
@@ -19,5 +22,24 @@ void getCountry(Country* cptr, int num);
 void displayCountry(Country* cptr, int num);
 
 int main (){
-    
+
 }
+
+void getCountry(Country* cptr, int num){
+    cout << "Country #" << num << "\n";
+    cout << "Name: ";
+    getline(cin, cptr->name);
+    cout << "Flight price from US: ";
+    cin >> cptr->flightPrice;
+    cout << "Duration in days: ";
+    cin >> cptr->duration;
+    cout <<"How many favorite sights? ";
+    cin >> cptr->numSights;
+
+    cptr -> favSights = new string[cptr->numSights];
+    for (int i = 0; i < cptr->numSights; i++){
+        cout <<"Favorite sight #"<<(i+1)<<": ";
+        getline(cin, *(cptr->favSights+i));
+    }
+    }
+
